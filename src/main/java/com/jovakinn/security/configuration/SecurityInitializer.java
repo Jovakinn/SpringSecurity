ba8b3c85152f75c2f9cf255797718b81789c2dc6
Jovakinn <max05012004@gmail.com>
2021-12-18 00:42:24 +0200

    Initial commit

---
+++ src/main/java/com/jovakinn/security/configuration/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.jovakinn.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
